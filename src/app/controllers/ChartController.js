59e4a004e65b18d4151a8acff90a37727c8ec9c3
Tung1862014 <Mayuri1862014@gmail.com>
2022-11-25 22:43:41 +0700

    44 edit general version2 #16

--- src/app/controllers/ChartController.js
+++ src/app/controllers/ChartController.js
@@ -194,105 +194,164 @@ class ChartController {
         let turnovers = [];
 
         if(req.query.type === 'tuan'){
+           
+            const dateto = new Date(req.query.ngde);
+            const day = dateto.getDate();
             
-            Promise.all([mydb.query(`SELECT * FROM donhang WHERE NB_id='${req.query.NB_id}' AND DH_ngay BETWEEN '${req.query.ngdi}' AND '${req.query.ngde}' AND DH_trangthai=4`)])
-               .then(([results]) =>{
-                   console.log('date results', results.length);
+            for(let i = 0; i < Number(day); i++){
+                const date = new Date(req.query.ngdi);
+                date.setDate(date.getDate() + i);
+                let YMD='';
+                let day = date.getDate();
+                let month = date.getMonth() + 1;
+                let year = date.getFullYear();
+
+                if (month < 10 && day >= 10) {
+                    YMD = year + '-0' + month + '-' + day;
+                } else if (month < 10 && day < 10) {
+                    YMD =  year + '-0' + month + '-0' + day;
+                } else if (month >= 10 && day < 10) {
+                    YMD =  year + '-' + month + '-0' + day;
+                } else if (month >= 10 && day >= 10) {
+                    YMD = year + '-' + month + '-' + day;
+                } else {
+                    YMD = year + '-' + month + '-' + day;
+                }
+                arr.push(YMD);
+            }
+            console.log('setDate: arr', arr);
+            for(let i=0; i < arr.length; i++){
+                Promise.all([ mydb.query(`SELECT sum(DH_tongtien) as money FROM donhang WHERE NB_id='${req.query.NB_id}' AND DH_ngay='${arr[i]}' AND DH_trangthai=4`)])
+                .then(([money]) =>{
+                                          
+                    if(money[0].money != null){
+                        turnovers =  [...turnovers,money[0].money];
+                    }else{
+                        turnovers =  [...turnovers,0];
+                    }
                    
-                   if(results.length > 0){
-                       for(let i=0; i<results.length; i++){
-                           let YMD='';
-                           let DateChart = new Date(results[i].DH_ngay);
-                               let day = DateChart.getDate();
-                               let month = DateChart.getMonth() + 1;
-                               let year = DateChart.getFullYear();
-       
-                               if (month < 10 && day >= 10) {
-                                   YMD = year + '-0' + month + '-' + day;
-                               } else if (month < 10 && day < 10) {
-                                   YMD =  year + '-0' + month + '-0' + day;
-                               } else if (month >= 10 && day < 10) {
-                                   YMD =  year + '-' + month + '-0' + day;
-                               } else if (month >= 10 && day >= 10) {
-                                   YMD = year + '-' + month + '-' + day;
-                               } else {
-                                   YMD = year + '-' + month + '-' + day;
-                               }
-                               //console.log(YMD);
-                               if(!arr.includes(YMD)){
-                                   arr.push(YMD);
-                               }
-                               // if(arr[0] === undefined){
-                               //     arr = [...arr,YMD];
-                               // }else{
-                               //     // for(let j=0; j < arr.length; j++){
-                               //     //     if(arr[j] !== (YMD)){
-                               //     //         if(j === arr.length-1){
-                               //     //             arr = [...arr,YMD];
-                               //     //         }
-                               //     //     }
-                               //     // }                             
-                               // }
-                       }
-                       console.log('day arr', arr);
-   
-                       for(let i=0; i<arr.length-1; i++){
-                           for(let j=i+1; j < arr.length; j++){
-                               if(handleTestDate(arr[i],arr[j])){
-                                   console.log(`arr${i}: `, arr[j]);
-                                   let temp = arr[i];
-                                   arr[i]= arr[j];
-                                   arr[j] = temp;
-                               }
-                           }
-                       }
-   
-   
-                       if(req.query.type == 'tuan'){
-                           for(let i=0; i < arr.length; i++){
-                               console.log('day', arr[i]);
-                               Promise.all([ mydb.query(`SELECT sum(DH_tongtien) as money FROM donhang WHERE NB_id='${req.query.NB_id}' AND DH_ngay='${arr[i]}' AND DH_trangthai=4`)])
-                                   .then(([money]) =>{
-                                      
-                                       turnovers =  [...turnovers,money[0].money];
-                                      
-                                       Promise.all([ mydb.query(`SELECT COUNT(DH_id) as orders FROM donhang WHERE NB_id='${req.query.NB_id}' AND DH_ngay='${arr[i]}' AND DH_trangthai=4`)])
-                                           .then(([orders]) =>{
-                                               //console.log('tong tien', turnovers);
-                                               numbers = [...numbers, orders[0].orders];
-                                               if(i === arr.length-1){
-                                                   console.log('arr', arr);
-                                                   console.log('chartListTurnover', turnovers);
-                                                   console.log('chartListNumber', numbers);
-                                                   res.json({
-                                                       arr: arr,
-                                                       turnovers: turnovers,
-                                                       numbers: numbers,
-                                                   })
-                                               }
-                                           })
-                                           .catch((err) =>{
-                                               console.log('loi nha');
-                                           })
-                                   })
-                                   .catch((err) =>{
-                                       console.log('loi');
-                                   })         
-                           }
-                       }
-                       
-                   }else{
-                       res.json({
-                           arr: [],
-                           turnovers: [],
-                           numbers: [],
-                       })
-                   }
-                   
-               }
-                   // res.json({numbers,results}),
-               )
-       }else {
+                    Promise.all([ mydb.query(`SELECT COUNT(DH_id) as orders FROM donhang WHERE NB_id='${req.query.NB_id}' AND DH_ngay='${arr[i]}' AND DH_trangthai=4`)])
+                        .then(([orders]) =>{
+                            //console.log('tong tien', turnovers);
+                            if(orders[0].orders != null){
+                                numbers = [...numbers, orders[0].orders];
+                            }else{
+                                numbers = [...numbers, 0];
+                            }
+                            if(i === arr.length-1){
+                                console.log('arr', arr);
+                                console.log('chartListTurnover', turnovers);
+                                console.log('chartListNumber', numbers);
+                                res.json({
+                                    arr: arr,
+                                    turnovers: turnovers,
+                                    numbers: numbers,
+                                })
+                            }
+                        })
+                        .catch((err) =>{
+                            console.log('loi nha');
+                        })
+                })
+                .catch((err) =>{
+                    console.log('loi');
+                })  
+            }
+                    
+        }else if(req.query.type == 'statistical'){
+            let datefrom = new Date(req.query.ngdi);
+            let dateto = new Date(req.query.ngde);
+            let monthFrom = datefrom.getMonth() + 1;
+            let monthTo = dateto.getMonth() + 1;
+            console.log('req.query.type: ', monthFrom, monthTo);
+
+            if(monthFrom.toString() == monthTo.toString()){
+                Promise.all([mydb.query(`SELECT * FROM donhang WHERE NB_id='${req.query.NB_id}' AND DH_ngay BETWEEN '${req.query.ngdi}' AND '${req.query.ngde}' AND DH_trangthai=4`)])
+                .then(([results]) =>{
+                    console.log('date results', results.length);
+                    
+                    if(results.length > 0){
+                        for(let i=0; i<results.length; i++){
+                            let YMD='';
+                            let DateChart = new Date(results[i].DH_ngay);
+                                let day = DateChart.getDate();
+                                let month = DateChart.getMonth() + 1;
+                                let year = DateChart.getFullYear();
+        
+                                if (month < 10 && day >= 10) {
+                                    YMD = year + '-0' + month + '-' + day;
+                                } else if (month < 10 && day < 10) {
+                                    YMD =  year + '-0' + month + '-0' + day;
+                                } else if (month >= 10 && day < 10) {
+                                    YMD =  year + '-' + month + '-0' + day;
+                                } else if (month >= 10 && day >= 10) {
+                                    YMD = year + '-' + month + '-' + day;
+                                } else {
+                                    YMD = year + '-' + month + '-' + day;
+                                }
+                                //console.log(YMD);
+                                if(!arr.includes(YMD)){
+                                    arr.push(YMD);
+                                }
+                        }
+                        console.log('day arr', arr);
+    
+                        for(let i=0; i<arr.length-1; i++){
+                            for(let j=i+1; j < arr.length; j++){
+                                if(handleTestDate(arr[i],arr[j])){
+                                    console.log(`arr${i}: `, arr[j]);
+                                    let temp = arr[i];
+                                    arr[i]= arr[j];
+                                    arr[j] = temp;
+                                }
+                            }
+                        }
+
+                        for(let i=0; i < arr.length; i++){
+                            console.log('day', arr[i]);
+                            Promise.all([ mydb.query(`SELECT sum(DH_tongtien) as money FROM donhang WHERE NB_id='${req.query.NB_id}' AND DH_ngay='${arr[i]}' AND DH_trangthai=4`)])
+                                .then(([money]) =>{
+                                   
+                                    turnovers =  [...turnovers,money[0].money];
+                                   
+                                    Promise.all([ mydb.query(`SELECT COUNT(DH_id) as orders FROM donhang WHERE NB_id='${req.query.NB_id}' AND DH_ngay='${arr[i]}' AND DH_trangthai=4`)])
+                                        .then(([orders]) =>{
+                                            //console.log('tong tien', turnovers);
+                                            numbers = [...numbers, orders[0].orders];
+                                            if(i === arr.length-1){
+                                                console.log('arr', arr);
+                                                console.log('chartListTurnover', turnovers);
+                                                console.log('chartListNumber', numbers);
+                                                res.json({
+                                                    arr: arr,
+                                                    turnovers: turnovers,
+                                                    numbers: numbers,
+                                                })
+                                            }
+                                        })
+                                        .catch((err) =>{
+                                            console.log('loi nha');
+                                        })
+                                })
+                                .catch((err) =>{
+                                    console.log('loi');
+                                })         
+                        }
+                        
+                    }else{
+                        res.json({
+                            arr: [],
+                            turnovers: [],
+                            numbers: [],
+                        })
+                    }
+                    
+                }
+            )
+        }else{
+
+        }
+       }else{
         const date = new Date();
         const dayValue = new Date();
         const year = date.getFullYear();
@@ -358,54 +417,55 @@ class ChartController {
                 }
             ]
         }else if(req.query.type == 'nam'){
+            console.log('nam nnn: ', req.query.nam);
             dateQuarter = [
                 {
-                    ngdi: `${year}-01-01`,
-                    ngde: `${year}-01-31`
+                    ngdi: `${req.query.nam}-01-01`,
+                    ngde: `${req.query.nam}-01-31`
                 },
                 {
-                    ngdi: `${year}-02-01`,
-                    ngde: `${year}-02-28`
+                    ngdi: `${req.query.nam}-02-01`,
+                    ngde: `${req.query.nam}-02-28`
                 },
                 {
-                    ngdi: `${year}-03-01`,
-                    ngde: `${year}-03-31`
+                    ngdi: `${req.query.nam}-03-01`,
+                    ngde: `${req.query.nam}-03-31`
                 },
                 {
-                    ngdi: `${year}-04-01`,
-                    ngde: `${year}-04-30`
+                    ngdi: `${req.query.nam}-04-01`,
+                    ngde: `${req.query.nam}-04-30`
                 },
                 {
-                    ngdi: `${year}-05-01`,
-                    ngde: `${year}-05-31`
+                    ngdi: `${req.query.nam}-05-01`,
+                    ngde: `${req.query.nam}-05-31`
                 },
                 {
-                    ngdi: `${year}-06-01`,
-                    ngde: `${year}-06-30`
+                    ngdi: `${req.query.nam}-06-01`,
+                    ngde: `${req.query.nam}-06-30`
                 },
                 {
-                    ngdi: `${year}-07-01`,
-                    ngde: `${year}-07-31`
+                    ngdi: `${req.query.nam}-07-01`,
+                    ngde: `${req.query.nam}-07-31`
                 },
                 {
-                    ngdi: `${year}-08-01`,
-                    ngde: `${year}-08-31`
+                    ngdi: `${req.query.nam}-08-01`,
+                    ngde: `${req.query.nam}-08-31`
                 },
                 {
-                    ngdi: `${year}-09-01`,
-                    ngde: `${year}-09-30`
+                    ngdi: `${req.query.nam}-09-01`,
+                    ngde: `${req.query.nam}-09-30`
                 },
                 {
-                    ngdi: `${year}-10-01`,
-                    ngde: `${year}-10-31`
+                    ngdi: `${req.query.nam}-10-01`,
+                    ngde: `${req.query.nam}-10-31`
                 },
                 {
-                    ngdi: `${year}-11-01`,
-                    ngde: `${year}-11-30`
+                    ngdi: `${req.query.nam}-11-01`,
+                    ngde: `${req.query.nam}-11-30`
                 },
                 {
-                    ngdi: `${year}-12-01`,
-                    ngde: `${year}-12-31`
+                    ngdi: `${req.query.nam}-12-01`,
+                    ngde: `${req.query.nam}-12-31`
                 }
             ]
         }
